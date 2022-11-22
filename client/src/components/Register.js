e28bb03edc7487ba2cdc4fd30c9609a7aa0dfce0
StalinGorod <114378450+StalinGorod@users.noreply.github.com>
2022-11-22 10:15:26 +0700

    Add files via upload

---
+++ client/src/components/Register.js
@@ -0,0 +1,67 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Axios from 'axios';
+
+function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [nama, setNama] = useState('');
+    const [erusername, setErusername] = useState('');
+    const [erpassword, setErpassword] = useState('');
+    const [ernama, setErnama] = useState('');
+
+    const register = () => {
+        if (username === '') {
+            setErusername('Mohon buat username')
+        } else if (password === '') {
+            setErpassword('Password harus diisi')
+            setErusername('')
+        } else if (nama === '') {
+            setErnama('Nama harus diisi')
+            setErpassword('')
+        } else {
+            Axios.post("http://localhost:3001/register", {
+                username: username,
+                password: password,
+                nama: nama
+            });
+        }
+    }
+
+    return (
+        <>
+            <div className="container py-5">
+                <h1 className='text-muted'>Register</h1>
+                <p className='text-muted'>
+                    Please register to add account
+                </p>
+                <hr />
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" className='form-control' onChange={(e) => { setUsername(e.target.value) }} />
+                    <b className="text-danger">{erusername}</b>
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className='form-control' onChange={(e) => { setPassword(e.target.value) }} />
+                    <b className="text-danger">{erpassword}</b>
+                </div>
+                <div className="form-group">
+                    <label>Name</label>
+                    <input type="text" className='form-control' onChange={(e) => { setNama(e.target.value) }} />
+                    <b className="text-danger">{ernama}</b>
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-danger my-3" onClick={register}>
+                        Register
+                    </button>
+                </div>
+                <p className="text-muted">
+                    Have an account! Please <Link to='/'>login</Link>
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Register;
